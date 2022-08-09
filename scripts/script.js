611cd579f5c6edc3493e854945eb4694628cd079
dorrin-sot <dorrinsotoudeh123@gmail.com>
2022-08-09 16:47:41 +0430

    save note on submit

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,39 @@
+const notes = []
+
+const addFieldWrapper = document.querySelector('.add-field-wrapper')
+const collapsed = document.querySelector('.add-field-wrapper > div.collapsed')
+const notCollapsed = document.querySelector('.add-field-wrapper > div:not(.collapsed)')
+
 function switchAddFieldCollapse() {
-  document.querySelector('.add-field-wrapper > div.collapsed')
-    .classList.toggle('hidden')
-  document.querySelector('.add-field-wrapper > div:not(.collapsed)')
-    .classList.toggle('hidden')
+  collapsed.classList.toggle('hidden')
+  notCollapsed.classList.toggle('hidden')
 }
+
+function saveListAndClearListUI() {
+  const title = notCollapsed.querySelector('#title');
+  const noteContent = notCollapsed.querySelector('#note > textarea');
+
+  if (!!title.value || !!noteContent.value) {
+    notes.push({
+      title: title.value,
+      content: noteContent.value
+    })
+  }
+
+  title.value = ''
+  noteContent.value = ''
+}
+
+function addList() {
+  saveListAndClearListUI()
+  switchAddFieldCollapse()
+  console.log(notes)
+}
+
+document.addEventListener('click', ({target}) => {
+  if (!addFieldWrapper.contains(target)) {
+    if (collapsed.classList.contains('hidden')) {
+      switchAddFieldCollapse()
+    }
+  }
+})
