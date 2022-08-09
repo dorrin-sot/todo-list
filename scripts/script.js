cea0c30b812eb0193af192b4063684029d3f8700
dorrin-sot <dorrinsotoudeh123@gmail.com>
2022-08-09 19:11:48 +0430

    delete note

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,19 @@
-const notes = [
-  {title: 'hello', 'content': 'no\n\nno'},
-  {title: 'hello', 'content': 'no'},
-  {title: 'hello', 'content': 'no'},
-  {title: 'hello', 'content': 'no'},
-  {title: 'hello', 'content': 'csdcksdncksal\njdscs\njskdc\nkjcsda\ncsd'},
-  {title: 'hello', 'content': 'csdcksdncksal\njdscs\njskdc\nkjcsda\ncsd'},
-  {title: 'hello', 'content': 'csdcksdncksal\njdscs\njskdc\nkjcsda\ncsd'},
-  {title: 'hello', 'content': 'no'},
-  {title: 'hello', 'content': 'no'},
-  {title: 'hello', 'content': 'csdcksdncksal\njdscs\njskdc\nkjcsda\ncsd'},
-  {title: 'hello', 'content': 'no'},
-  {title: 'hello', 'content': 'no'},
-  {title: 'hello', 'content': 'csdcksdncksal\njdscs\njskdc\nkjcsda\ncsd'},
-  {title: 'hello', 'content': 'no'},
-  {title: 'hello', 'content': 'csdcksdncksal\njdscs\njskdc\nkjcsda\ncsd'},
+let notes = [
+  {id: 0,title: 'hello', 'content': 'no\n\nno'},
+  {id: 1,title: 'hello', 'content': 'no'},
+  {id: 2,title: 'hello', 'content': 'no'},
+  {id: 3,title: 'hello', 'content': 'no'},
+  {id: 4,title: 'hello', 'content': 'csdcksdncksal\njdscs\njskdc\nkjcsda\ncsd'},
+  {id: 5,title: 'hello', 'content': 'csdcksdncksal\njdscs\njskdc\nkjcsda\ncsd'},
+  {id: 6,title: 'hello', 'content': 'csdcksdncksal\njdscs\njskdc\nkjcsda\ncsd'},
+  {id: 7,title: 'hello', 'content': 'no'},
+  {id: 8,title: 'hello', 'content': 'no'},
+  {id: 9,title: 'hello', 'content': 'csdcksdncksal\njdscs\njskdc\nkjcsda\ncsd'},
+  {id: 10,title: 'hello', 'content': 'no'},
+  {id: 11,title: 'hello', 'content': 'no'},
+  {id: 12,title: 'hello', 'content': 'csdcksdncksal\njdscs\njskdc\nkjcsda\ncsd'},
+  {id: 13,title: 'hello', 'content': 'no'},
+  {id: 14,title: 'hello', 'content': 'csdcksdncksal\njdscs\njskdc\nkjcsda\ncsd'},
 ]
 
 const addFieldWrapper = document.querySelector('.add-field-wrapper')
@@ -33,7 +33,8 @@ function saveListAndClearListUI() {
   if (!!title.value || !!noteContent.value) {
     notes.push({
       title: title.value,
-      content: noteContent.value
+      content: noteContent.value,
+      id: (notes[notes.length - 1]|| {id: -1}) .id + 1
     })
   }
 
@@ -48,11 +49,21 @@ function updateNotesGrid() {
 
   for (const note of notes) {
     notesGrid.innerHTML +=
-      `<div class="note">
-        <h2>${note.title}</h2>
-        <pre>${note.content}</pre>
+      `<div class="note-wrapper">
+        <div class="note">
+          <h2>${note.title}</h2>
+          <pre>${note.content}</pre>
+        </div>
+        <div class="overlay">
+          <i class="icon-button far fa-trash-can" onclick="deleteNote(${note.id})"></i>
+        </div>
       </div>`
   }
+}
+
+function deleteNote(id) {
+  notes = notes.filter((n) => n.id !== id)
+  updateNotesGrid()
 }
 
 function addList() {
